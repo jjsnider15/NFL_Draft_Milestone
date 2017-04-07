4074086342474bb9a413734142fa850208eb7ac3
Justin Snider <jjsnider15@gmail.com>
2017-04-07 06:02:36 -0400

    Submitting all Milestone Assignments at once.

---
+++ milestone_draft.java
@@ -0,0 +1,19 @@
+public class milestone_draft{
+	public static void main(String[] args){
+		
+		player p1 = new player();
+		player p2 = new player();
+		player p3 = new player();
+		player p4 = new player();
+		
+		p1.stats = 1137;
+		p2.stats = 1014;
+		p3.stats = 1004;
+		p4.stats = 735;
+				
+		
+		
+		
+		
+	}
+}
---
+++ player.java
@@ -0,0 +1,17 @@
+public class player {
+
+	String name;
+	int stats;
+	public void setName(String n){
+		name = n;
+	}
+public String getName(){
+		return name;
+	}
+public void setPoints(int s){
+	stats = s;
+}
+public int getPoints(){
+	return stats;
+}
+}
